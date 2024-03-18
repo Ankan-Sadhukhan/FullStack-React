cab44766d03ff8c0cf825a94e7e1eba84a7cc133
Ankan-Sadhukhan <sadhukhanankan7@gmail.com>
2024-03-19 01:22:25 +0530

    clean up file

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,13 @@
 
 import './App.css';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import axios from 'axios';
+import Home from './pages/Home';
 
 function App() {
 
-    const [listOfPosts, setlistOfPosts] = useState([]);
-
-    useEffect(() => {
-      axios.get('http://localhost:3001/api/v1/posts').then((response) => {
-        setlistOfPosts(response.data);
-      });
-    }, []);
+   
   return (
     <div className="App">
-      {listOfPosts.map((value, key) => {
-        return (
-          <div className="post">
-            <div className="title"> {value.title} </div>
-            <div className="body">{value.postText}</div>
-            <div className="footer">{value.username}</div>
-          </div>
-        );
-      })}
+      <Home />
     </div>
   );
 }
