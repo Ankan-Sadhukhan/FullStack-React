300eed14895ab5f87959b4df8fd05830674e3071
Ankan-Sadhukhan <sadhukhanankan7@gmail.com>
2024-03-19 01:53:05 +0530

    implement basic react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-
 import './App.css';
 import Home from './pages/Home';
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 
 function App() {
 
+  const appRouter= createBrowserRouter([
+    {
+        path:"/",
+        element:<Home />
+        
+    
+    },
+    
+])
    
   return (
     <div className="App">
-      <Home />
+      <RouterProvider router={appRouter}>
+      </RouterProvider>
     </div>
   );
 }
