e493ffdd5cf3fa8723d8cd137471b48d3aee4511
Ankan-Sadhukhan <sadhukhanankan7@gmail.com>
2024-03-19 02:09:48 +0530

    Implement create post using formik library and used yup package for form validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
 import './App.css';
 import Home from './pages/Home';
-import {createBrowserRouter,RouterProvider} from 'react-router-dom'
-
+import {createBrowserRouter,RouterProvider,Link} from 'react-router-dom'
+import CreatePost from './pages/CreatePost';
 function App() {
 
   const appRouter= createBrowserRouter([
     {
         path:"/",
         element:<Home />
-        
-    
     },
+    {
+      path:"/createpost",
+      element:<CreatePost />
+    }
     
 ])
    
   return (
     <div className="App">
+      <a href="/createpost"> Create A Post</a>
+      <a href="/"> Home Page</a>
       <RouterProvider router={appRouter}>
       </RouterProvider>
     </div>
